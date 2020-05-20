5d222f236999e35ad0d5efe3b2210d83d083e2e9
Thijsvanede <t.s.vanede@utwente.nl>
2020-05-20 15:36:55 -0700

    Added setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="argparse-formatter",
+    version="0.0.1",
+    author="Thijs van Ede",
+    author_email="t.s.vanede@utwente.nl",
+    description="Provides a structured formatter for the argparse module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Thijsvanede/StructuredFormatter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
