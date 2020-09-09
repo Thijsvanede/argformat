d87e2692c87496929f51feb5d33d932c755d3086
Thijs van Ede <thijsvanede@hotmail.com>
2020-09-09 11:05:32 +0200

    Push new pip version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="argformat",
-    version="0.0.2",
+    version="0.0.3",
     author="Thijs van Ede",
     author_email="t.s.vanede@utwente.nl",
     description="Provides a structured formatter for the argparse module",
